a87363e6f014132f76441d3128422a861dd47188
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-08-01 17:27:49 -0500

    cambio en columnas para ipad y tamaño heder fijo

--- themes/organics-child/plugins/plugin.woocommerce.php
+++ themes/organics-child/plugins/plugin.woocommerce.php
@@ -647,7 +647,7 @@ if ( !function_exists( 'organics_woocommerce_close_item_wrapper' ) ) {
 			        /*  Add to cart Button  */
 			        ?><div class="shortcode_add_to_button"><?php
 			        echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-			            sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="icon-shopping-cart13 button %s product_type_%s">%s</a>',
+			            sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="icon-shopping-cart13 button %s product_type_%s">Añadir al carrito</a>',
 			                esc_url( $product->add_to_cart_url() ),
 			                esc_attr( $product->id ),
 			                esc_attr( $product->get_sku() ),
