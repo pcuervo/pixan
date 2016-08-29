7c46f18d954d39512c488be5db060da3fd854cda
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-08-29 11:39:36 -0500

    cambio de texto checkout

--- themes/organics-child/woocommerce/checkout/form-billing.php
+++ themes/organics-child/woocommerce/checkout/form-billing.php
@@ -30,7 +30,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<?php else : ?>
 
-		<h3><?php _e( 'Detalles de compra', 'woocommerce' ); ?></h3>
+		<h3><?php _e( 'Dirección de envío', 'woocommerce' ); ?></h3>
 
 	<?php endif; ?>
 
