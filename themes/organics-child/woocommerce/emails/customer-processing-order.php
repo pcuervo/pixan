5dd162fb0f650b682531a0e3f88616c5014a8502
Jonas Graterol <jonas@pcuervo.com>
2017-01-30 12:17:06 -0600

    Agregar fecha de envio y fecha de cancelacion al correo de confirmacion de pedido

--- themes/organics-child/woocommerce/emails/customer-processing-order.php
+++ themes/organics-child/woocommerce/emails/customer-processing-order.php
@@ -25,7 +25,65 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<p><?php _e( "Gracias por comprar en Pixan. Tu pedido se ha recibido y se estará entregando el día XXXX. Los detalles del pedido son los siguientes:", 'woocommerce' ); ?></p>
+<?php
+	//CALCULAR EL DIA DE ENTREGA
+	//var_dump($order);
+	$semana = array(
+				'Lunes' 	=> 'monday',
+				'Martes' 	=> 'tuesday',
+				'Miercoles' => 'wednesday',
+				'Jueves' 	=> 'thursday',
+				'Viernes' 	=> 'friday',
+				'Sabado' 	=> 'saturday',
+				'Domingo' 	=> 'sunday',
+				);
+	$meses = array(
+				'January' => 'Enero',
+			    'February' => 'Febrero',
+			    'March' => 'Marzo',
+			    'April' => 'Abril',
+			    'May' => 'Mayo',
+			    'June' => 'Junio',
+			    'July ' => 'Julio',
+			    'August' => 'Agosto',
+			    'September' => 'Septiembre',
+			    'October' => 'Octubre',
+			    'November' => 'Noviembre',
+			    'December' => 'Diciembre'
+				);
+	$meta_orden = get_post_meta($order->id);
+	//var_dump($meta_orden);
+	$meta = get_post_meta($meta_orden["_billing_area_entrega"][0]);
+	//var_dump($meta);
+	$dias = '';
+	if(isset($meta['_dia1'])) { $dias .= $meta['_dia1'][0].', '; }
+	if(isset($meta['_dia2'])) { $dias .= $meta['_dia2'][0].', '; }
+	if(isset($meta['_dia3'])) { $dias .= $meta['_dia3'][0].', '; }
+	if(isset($meta['_dia4'])) { $dias .= $meta['_dia4'][0].', '; }
+	if(isset($meta['_dia5'])) { $dias .= $meta['_dia5'][0].', '; }
+	if(isset($meta['_dia6'])) { $dias .= $meta['_dia6'][0].', '; }
+	if(isset($meta['_dia7'])) { $dias .= $meta['_dia7'][0].', '; }
+
+
+	$dias = substr($dias, 0, -2);
+	$d = explode(',', $dias);
+	//OBTENER FECHA DE PROXIMA ENTREGA
+	$timestamp = strtotime('+1 day');
+
+	$dia = date('d', strtotime("next ".$semana[$d[0]] . date('H:i:s', $timestamp), $timestamp));
+	$m = date('F', strtotime("next ".$semana[$d[0]] . date('H:i:s', $timestamp), $timestamp));
+	$a = date('Y', strtotime("next ".$semana[$d[0]] . date('H:i:s', $timestamp), $timestamp));
+	$p = $d[0].' '.$dia.' de '.$meses[$m].', '.$a;
+
+	$timestamp_c = strtotime('-2 days');
+	$dia_c = date('d', strtotime("next ".$semana[$d[0]] . " -1 day", $timestamp));
+	$m_c = date('F', strtotime("next ".$semana[$d[0]] . " -1 day", $timestamp));
+	$a_c = date('Y', strtotime("next ".$semana[$d[0]] . " -1 day", $timestamp));
+	$cance = $dia_c.' de '.$meses[$m_c].', '.$a_c;
+	setlocale(LC_ALL,"es_ES");
+?>
+
+<p><?php _e( "Gracias por comprar en Pixan. Tu pedido se ha recibido y se estará entregando el día <strong>".$p."</strong>. Recuerda que la fecha máxima para la cancelación de tu pedido es el día <strong>".$cance."</strong>. Los detalles del pedido son los siguientes:", 'woocommerce' ); ?></p>
 
 <?php
 
