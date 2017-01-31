bc63e8ef26f257552e05b3bf0e52d64d29cbac67
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2017-01-31 12:36:28 -0600

    estilo lista pedidos

--- themes/organics-child/includes/js/print-list.js
+++ themes/organics-child/includes/js/print-list.js
@@ -1,18 +1,77 @@
 
 $("#btnImprimir").click(function(e) {
 	e.preventDefault();
-  $('.screen-reader-text, .order_actions, .order_actions').hide();
+  $('.screen-reader-text, .order_actions, .order_actions, .edit, .trash, .button.tips, input ').hide();
+  $('#the-list a, small, .meta').css({
+    color: "#000",
+    textDecoration: "none",
+    marginRight: "10px",
+  });
+  $('*').css({
+    fontWeight: "500",
+    fontFamily: "Arial",
+    lineheight: "30px",
+    fontSize: "14px",
+    marginBottom: "5px"
+  });
+  $('.show_order_items').css({
+    backgroundColor: "#fff",
+    marginLeft: "-15px",
+    marginTop: "-15px",
+    height: "20px"
+  });
+  $('input[type=checkbox]').css( "border-color", "#fff" );
+  $('.email').css({
+   marginBottom: "-20px",
+   backgroundColor: "#fff",
+   zIndex: "99",
+   position: "relative"
+  });
+  $('.row-title').css( "margin-left", "20px" );
+  $('.amount, .meta, abbr, .note-on.tips').css( "display", "block" );
+  $('.row-title').css( "clear", "both" );
+  $('.order_status').css( "background-color", "#fff" );
   $(':checkbox').hide();
+
   setTimeout(function() {
     imprimirDiv();
     setTimeout(function() {
-      $('.screen-reader-text, .order_actions, .order_actions').show();
+      $('.screen-reader-text, .order_actions, .order_actions, .edit, .trash, .button.tips, input').show();
+      $('#the-list a, small, .meta').css({
+        color: "",
+        textDecoration: "",
+        marginRight: "",
+      });
+      $('*').css({
+        fontWeight: "",
+        fontFamily: "",
+        lineheight: "",
+        fontSize: "",
+        marginBottom: ""
+      });
+      $('.show_order_items').css({
+        backgroundColor: "#",
+        marginLeft: "",
+        marginTop: "",
+        height: ""
+      });
+      $('input[type=checkbox]').css( "border-color", "" );
+      $('.email').css({
+       marginBottom: "",
+       backgroundColor: "",
+       zIndex: "",
+       position: ""
+      });
+      $('.row-title').css( "margin-left", "" );
+      $('.amount, .meta, abbr, .note-on.tips').css( "display", "" );
+      $('.row-title').css( "clear", "" );
+      $('.order_status').css( "background-color", "" );
       $(':checkbox').show();
     }, 1000);
   }, 1000);
 });
 
-function imprimirDiv() 
+function imprimirDiv()
 {
   //var ficha = document.getElementsByClassName("wp-list-table widefat fixed striped posts");
   //var ficha = $('.wp-list-table');
@@ -25,5 +84,5 @@ function imprimirDiv()
   ventimp.document.close();
   ventimp.print( );
   ventimp.close();
-    
+
 }
