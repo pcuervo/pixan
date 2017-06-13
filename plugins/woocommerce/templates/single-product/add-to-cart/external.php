33c52a3b8151b807c0bd8ca9d0d5f400778dd325
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2017-06-13 14:36:49 -0500

    actualización woocommerce

--- plugins/woocommerce/templates/single-product/add-to-cart/external.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/external.php
@@ -15,17 +15,14 @@
  * @package 	WooCommerce/Templates
  * @version     2.1.0
  */
-
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
-?>
-
-<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 <p class="cart">
-	<a href="<?php echo esc_url( $product_url ); ?>" rel="nofollow" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></a>
+	<a href="<?php echo esc_url( $product_url ); ?>" rel="nofollow" class="single_add_to_cart_button button alt"><?php echo esc_html( $button_text ); ?></a>
 </p>
 
 <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
